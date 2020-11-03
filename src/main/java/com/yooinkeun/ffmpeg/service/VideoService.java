c028582215946dba7fabb9a1ef2ba50c05414b06
YooInKeun <keun0390@naver.com>
2020-11-04 00:13:09 +0900

    feat: add log video play time with ffmpeg

--- src/main/java/com/yooinkeun/ffmpeg/service/VideoService.java
+++ src/main/java/com/yooinkeun/ffmpeg/service/VideoService.java
@@ -6,13 +6,19 @@ import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.yooinkeun.ffmpeg.config.S3Config;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.bramp.ffmpeg.FFprobe;
+import net.bramp.ffmpeg.probe.FFmpegFormat;
+import net.bramp.ffmpeg.probe.FFmpegProbeResult;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.transaction.Transactional;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.UUID;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class VideoService {
@@ -21,6 +27,7 @@ public class VideoService {
 
     private final AmazonS3 amazonS3;
 
+    @Transactional
     public String upload(MultipartFile file) throws IOException {
         String uuid = UUID.randomUUID().toString();
         Path basePath = s3Config.getBasePath();
@@ -34,7 +41,20 @@ public class VideoService {
                 .withCannedAcl(CannedAccessControlList.PublicRead);
 
         amazonS3.putObject(putObjectRequest);
-        return amazonS3.getUrl(s3Config.getBucketName(), key).toString();
+        String fileUrl = amazonS3.getUrl(s3Config.getBucketName(), key).toString();
+        logVideoPlayTile(fileUrl);
+        return fileUrl;
+    }
+
+    private void logVideoPlayTile(String fileUrl) {
+        try {
+            FFprobe ffprobe = new FFprobe(fileUrl);  //리눅스에 설치되어 있는 ffmpeg 폴더
+            FFmpegProbeResult probeResult = ffprobe.probe("");
+            FFmpegFormat format = probeResult.getFormat();
+            log.info(Double.toString(format.duration));    //초단위
+        } catch(IOException e) {
+            log.error("error", e);
+        }
     }
 
     private ObjectMetadata makeMetaData(MultipartFile multipartFile) {
